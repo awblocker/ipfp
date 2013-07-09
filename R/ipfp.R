f0a0dc52eadd6a089279cdf23e70f70f8590e0d9
Alexander W Blocker <ablocker@gmail.com>
2013-07-08 20:43:46 -0700

    Add Apache 2.0 license information. Update IPFP information.

--- R/ipfp.R
+++ R/ipfp.R
@@ -1,7 +1,21 @@
-#' Function to run basic IPFP (iterative proportional fitting procedure)
+# Copyright 2012 Alexander W Blocker
+#    
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' Function to run IPFP (iterative proportional fitting procedure)
 #'
 #' Use IPFP starting from x0 to produce vector x s.t. Ax = y within tolerance.
-#' Need to ensure that x0 >= 0.
+#' Need to ensure that x0 > 0.
 #'
 #' @param y numeric constraint vector (length nrow)
 #' @param A constraint matrix (nrow x ncol)
@@ -10,14 +24,14 @@
 #' @param maxit integer maximum number of iterations for IPFP; defaults to 1e3
 #' @param verbose logical parameter to select verbose output from C function
 #' @param full logical parameter to select full return (with diagnostic info)
-#' @return if not full, vector of length ncol containing solution obtained by
-#'      IPFP. If full, list containing solution (as x), number of iterations (as
-#'      iter), and norm of Ax - y (as errNorm)
+#' @return if not full, a vector of length ncol containing solution obtained by
+#'      IPFP. If full, a list containing solution (as x), the number of iterations
+#'      (as iter), and the L2 norm of Ax - y (as errNorm)
 #' @keywords iteration array
 #' @export
-#' @useDynLib networkTomography
+#' @useDynLib ipfp
 #' @examples
-#' A <- buildStarMat(3)
+#' A <- matrix(c(1,0,0, 1,0,0, 0,1,0, 0,1,0, 0,0,1), nrow=3)
 #' x <- rgamma(ncol(A), 10, 1/100)
 #' y <- A %*% x
 #' x0 <- x * rgamma(length(x), 10, 10)
@@ -42,7 +56,7 @@ ipfp <- function(y, A, x0,
     ans <- .Call("ipfp", y[activeRows], A[activeRows, activeCols, drop=FALSE],
             dim(A[activeRows, activeCols, drop=FALSE]), x0[activeCols],
             as.numeric(tol), as.integer(maxit), as.logical(verbose),
-            PACKAGE='networkTomography')
+            PACKAGE='ipfp')
     
     x0[activeCols] <- ans$x
     
